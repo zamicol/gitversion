e3e0bff72523661eeef8c49baa87cb128cfb1c06
Zamicol <zachary_collier@comcast.com>
2016-10-03 13:54:58 -0600

    return errors

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 )
 
 // Get uses git to construct a version string using tag and commit.
+// This can be useful where deployed go binaries versioning is important.
+//
 //
 // For a clean git commit, Get() will return a simple hash.
 //
@@ -22,13 +24,13 @@ import (
 // If there is tag information, the tag name will be prepended before the
 // commit hash.
 //
-//     v1.0 026249145dab6c65dbfeedf7d01aa2720f51a815
+//     v1.0.0 026249145dab6c65dbfeedf7d01aa2720f51a815
 //
 // Or if there are uncommited changes:
 //
-//     v1.0 026249145dab6c65dbfeedf7d01aa2720f51a815 (uncommited)
+//     v1.0.0 026249145dab6c65dbfeedf7d01aa2720f51a815 (uncommited)
 //
-func Get() string {
+func Get() (string, error) {
 
 	////////////////////
 	// commit hash
@@ -38,8 +40,7 @@ func Get() string {
 	cmd.Stdout = &out
 	err := cmd.Run()
 	if err != nil {
-		err = errors.New("git command failed.  (Need to 'git init' and commit?)" + err.Error())
-		panic(err)
+		return "", errors.New("gitversion: git command failed.  (Need to 'git init' and commit?)")
 	}
 
 	// git log will return a few lines, We only want the line about commit.
@@ -47,8 +48,7 @@ func Get() string {
 	matches := reg.FindStringSubmatch(out.String())
 
 	if len(matches) != 2 || len(matches[1]) != 40 {
-		err = errors.New("Problem getting git commit. ")
-		panic(err)
+		return "", errors.New("gitversion: unable to get git commit hash")
 	}
 
 	hash := matches[1]
@@ -56,13 +56,13 @@ func Get() string {
 	////////////////////
 	// tag
 	////////////////////
-	cmd = exec.Command("git", "tag") // If there are no tags, git returns nothing
+	cmd = exec.Command("git", "tag", "--sort=-taggerdate") // If there are no tags, git returns nothing
 
 	out.Reset()
 	cmd.Stdout = &out
 	err = cmd.Run()
 	if err != nil {
-		panic(err)
+		return "", errors.New("gitversion: problem with git tag")
 	}
 
 	reg = regexp.MustCompile(`\A.*`) // Grab first line
@@ -79,7 +79,7 @@ func Get() string {
 	cmd.Stdout = &out
 	err = cmd.Run()
 	if err != nil {
-		panic(err)
+		return "", errors.New("gitversion: problem with git status")
 	}
 
 	reg = regexp.MustCompile(`(Changes to be committed)|(Changes not staged for commit)`)
@@ -91,5 +91,5 @@ func Get() string {
 	////////////////////
 	// construct our version and return.
 	////////////////////
-	return tag + hash + uncommited
+	return tag + hash + uncommited, nil
 }
